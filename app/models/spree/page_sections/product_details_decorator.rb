fbff6a9fe05da0d337324a3d1a3d39aaa60b79fe
jatin-rajput <rajputj829@gmail.com>
2026-04-16 08:44:25 +0530

    update version and fix bug

--- app/models/spree/page_sections/product_details_decorator.rb
+++ app/models/spree/page_sections/product_details_decorator.rb
@@ -1,15 +1,17 @@
 module Spree
-    module PageSections
-      module ProductDetailsDecorator
-        def default_blocks
-          super + [Spree::PageBlocks::Products::RazorpayAffordability.new]
-        end
-  
-        def available_blocks_to_add
-          super + [Spree::PageBlocks::Products::RazorpayAffordability]
-        end
+  module PageSections
+    module ProductDetailsDecorator
+      def default_blocks
+        super + [Spree::PageBlocks::Products::RazorpayAffordability.new]
+      end
+
+      def available_blocks_to_add
+        super + [Spree::PageBlocks::Products::RazorpayAffordability]
       end
     end
   end
-  
-Spree::PageSections::ProductDetailsDecorator.prepend(Spree::PageSections::ProductDetailsDecorator)
+end
+
+  # Spree::PageSections::ProductDetails.prepend(
+  #   Spree::PageSections::ProductDetailsDecorator
+  # )
